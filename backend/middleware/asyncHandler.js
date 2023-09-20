901067ae429e6441f73de21044668241bd2a3478
asalas <67822688+andrewjsalas@users.noreply.github.com>
2023-09-20 12:28:40 -0700

    Bug fixes and routes
    Fixed a bug where my middleware wasn't being imported correctly. This was resolved by adding '.js' after the file import. Further streamlined the routes and created controllers to handle the route functionality. Also created basic error handling middleware and added them to the routes as well.

--- backend/middleware/asyncHandler.js
+++ backend/middleware/asyncHandler.js
@@ -1,7 +1,6 @@
 
 // Handles async await to eliminate having to write several try catches for every route. 
-const asyncHandler = fn => (req, res, next) => {
+const asyncHandler = (fn) => (req, res, next) => {
     Promise.resolve(fn(req, res, next)).catch(next);
-}
-
+};
 export default asyncHandler;
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,25 +1,8 @@
 import express from 'express';
 const router = express.Router();
-import asyncHandler from '../middleware/asyncHandler';
-import Product from '../models/productModel';
+import { getProducts, getProductById } from '../controllers/productController.js';
 
-// Get all products
-router.get('/', asyncHandler(async (req, res) => {
-    // Pass in empty object to find all products
-    const products = await Product.find({});
-    res.json(products);
-}));
-
-// Get single product by ID
-router.get('/:id', asyncHandler(async(req, res) => {
-    const product = await Product.findById(req.params.id);
-
-    if(product) {
-        res.json(product);
-    }
-
-    res.status(404).json({ message: 'Product not found' });
-    res.json(product);
-}));
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 
 export default router;
